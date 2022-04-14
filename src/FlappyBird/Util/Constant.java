aaed72588edb8581e6dc9c6d85389e6c1c711f8c
tuquyn <ndtquyen147@gmail.com>
2022-04-14 23:00:08 +0700

    create frame

---
+++ src/FlappyBird/Util/Constant.java
@@ -0,0 +1,17 @@
+package FlappyBird.Util;
+
+import javax.swing.*;
+
+public class Constant {
+    // Window size
+    public static final int frameWidth = 1024;
+    public static final int frameHeight = 512;
+
+    public static final int frameX = 250;
+    public static final int frameY = 200;
+
+    public static final ImageIcon img = new ImageIcon("src/resources/icon.png");
+
+    public static final String frameTitle = "Flappy Bird made by We Showed up!";
+    public static final int FPS = 60;
+}
