1b7448e987e213a3ca4c199a659043c7aaddfbd7
tuchihuynhxjulie <100422136+tuchihuynhxjulie@users.noreply.github.com>
2022-05-07 20:26:36 +0700

    Create Sound.java

---
+++ src/FlappyBird/Control/Sound.java
@@ -0,0 +1,23 @@
+package FlappyBird.Control;
+
+import FlappyBird.Util.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import static FlappyBird.Util.Constant.*;
+
+public class Sound {
+    private AudioClip soundEffect;
+
+    public Sound(String filePath) {
+        soundEffect = new AudioClip(getClass().getResource(filePath).toExternalForm());
+    }
+
+    public void playClip() {
+        soundEffect.play();
+    }
+}
+        
+    
+    
