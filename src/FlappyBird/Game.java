54f94f85a28d404ba2c0f1f92a6e8faa516e3551
tuchihuynhxjulie <100422136+tuchihuynhxjulie@users.noreply.github.com>
2022-05-07 20:35:45 +0700

    Update Game.java

--- src/FlappyBird/Game.java
+++ src/FlappyBird/Game.java
@@ -3,6 +3,7 @@ package FlappyBird;
 import FlappyBird.Control.*;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
